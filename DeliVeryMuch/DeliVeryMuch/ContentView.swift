5ac783c3c1664ee9488f18f13ca4632909308a34
Federico Diaz <federico.dario.diaz@gmail.com>
2025-02-06 16:06:25 -0300

    project created

---
+++ DeliVeryMuch/DeliVeryMuch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeliVeryMuch
+//
+//  Created by Federico Díaz on 06/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeliVeryMuch/DeliVeryMuch/DeliVeryMuchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliVeryMuchApp.swift
+//  DeliVeryMuch
+//
+//  Created by Federico Díaz on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeliVeryMuchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
