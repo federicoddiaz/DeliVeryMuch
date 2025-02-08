bdda2c63a60e00babfe7756df0687f29d84acd3f
Federico Diaz <federico.dario.diaz@gmail.com>
2025-02-08 18:38:25 -0300

    moved files

---
+++ DeliVeryMuch/DeliVeryMuch/Views/Cells/ProductListCellView.swift
@@ -0,0 +1,37 @@
+//
+//  ProductListCellView.swift
+//  DeliVeryMuch
+//
+//  Created by Federico Díaz on 06/02/2025.
+//
+
+import SwiftUI
+
+struct ProductListCellView: View {
+    
+    let product: Product
+    
+    var body: some View {
+        HStack {
+            ProductRemoteImage(urlString: product.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 90)
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(product.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                
+                Text("$\(product.price, specifier: "%.2f")")
+                    .foregroundStyle(.secondary)
+                    .fontWeight(.semibold)
+            }
+            .padding(.leading)
+        }
+    }
+}
+
+#Preview {
+    ProductListCellView(product: MockData.sampleProduct)
+}
