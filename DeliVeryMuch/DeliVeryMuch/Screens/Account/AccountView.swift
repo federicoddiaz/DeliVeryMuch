b21737d24c0c561a1f84f924bf51d5aef9704b45
Federico Diaz <federico.dario.diaz@gmail.com>
2025-02-08 19:35:42 -0300

    account screen ui & basic logic implemented

--- DeliVeryMuch/DeliVeryMuch/Screens/Account/AccountView.swift
+++ DeliVeryMuch/DeliVeryMuch/Screens/Account/AccountView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @StateObject var viewModel = AccountViewModel()
+    
     var body: some View {
         NavigationStack {
-            Text("Account View")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $viewModel.user.firstName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
+                    TextField("Email", text: $viewModel.user.email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birthday", selection: $viewModel.user.dateOfBirth, displayedComponents: .date)
+                    
+                    Button {
+                        viewModel.saveChanges()
+                    } label: {
+                        Text("Save changes")
+                    }
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle("Extra Napkings", isOn: $viewModel.user.extraNapkins)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
+                } header: {
+                    Text("User Preferences")
+                }
+                
+            }
+            .tint(.brandPrimary)
+            .navigationTitle("Account")
+        }
+        .onAppear {
+            viewModel.getUser()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
